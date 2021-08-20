bcc472bcf06764e1d4b1ff35c31b9eb5a4a28e26
Kevin Mcguigan <36888812+kevinkmcguigan@users.noreply.github.com>
2021-08-20 13:06:34 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='numfu',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='kevin.mcguigan@nscc.ca',
+    author='kevin mcguigan',
+    author_email='kevin.mcguigan@nscc.ca',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
