862883e43e61d2268ba44cb36f25c70f59a7e3bd
McGuigan <kevin.mcguigan@nscc.ca>
2021-10-22 10:59:05 -0300

    basic setup

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
-from setuptools import setup
+import setuptools
 
-setup(
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
     name='numfu',
+    packages = ['numfu'],
     version='0.0.1',
-    description='',
+    license='MIT',
+    description='Master the numpy arts',
     long_description='',
-    url='kevin.mcguigan@nscc.ca',
+    url='',
+    download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
     author='kevin mcguigan',
-    author_email='kevin.mcguigan@nscc.ca',
-    packages=[],
+    author_email='',
+    keywords = ['numpy', 'snippits'], 
+    install_requires=['numpy'],
     classifiers=['Development Status :: 1 - Planning'],
 )
+
+# more info:
+# https://packaging.python.org/tutorials/packaging-projects/
+
+# better:
+#https://medium.com/@joel.barmettler/how-to-upload-your-python-package-to-pypi-65edc5fe9c56
